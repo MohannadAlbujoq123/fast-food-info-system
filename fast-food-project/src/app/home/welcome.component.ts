0adb5d9df964507d9b9b7f21685c3d6bfcde5553
Mohannad Al-Bujoq <MAlBujoq@EpyaSolutions.com>
2024-12-04 11:53:29 +0300

    Remove old project files and make new initial commit

--- fast-food-project/src/app/home/welcome.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './welcome.component.html',
-  standalone: false,
-})
-export class WelcomeComponent {
-  public pageTitle = 'Welcome';
-}
--- fast-food-project/src/app/products/product.ts
+++
@@ -1,9 +0,0 @@
-export interface IProduct {
-    productId: number;
-    productName: string;
-    productCode: string;
-    price: number;
-    description: string;
-    starRating: number;
-    imageUrl: string;
-}
